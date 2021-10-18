eb4d6855bcb106194a3bb2bbe1fcfdcb52f98e27
Hakan Girgin <hakangirgin7@gmail.com>
2021-10-18 22:04:15 +0100

    18.10.2021 last commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("Merhaba kodlamaio")
+
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
+
+console.log(dolarDun)
+
+let konutKredileri =["Konut kredisi", "Emlak konut kredisi","Kamu konut kredisi"];
+console.log(konutKredileri)
